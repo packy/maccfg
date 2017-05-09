58bbbe60e966880aefb1d4ae62586608fd969028
Packy Anderson <packy@cpan.org>
2017-05-09 11:49:50 -0400

    Fix dependencies in the ack makefile

--- ack2.mk
+++ ack2.mk
@@ -3,7 +3,7 @@ ack: ${gitdir}/ack2/ack-standalone
 ${gitdir}/ack2:
 	git clone https://github.com/packy/ack2.git ${gitdir}/ack2
 
-${gitdir}/ack2/Makefile: $(DEPDIR)/homebrew $(PERLBREW) $(DEPDIR)/File-Next
+${gitdir}/ack2/Makefile: $(DEPDIR)/homebrew $(PERLBREW) $(DEPDIR)/File-Next ${gitdir}/ack2
 	cd ${gitdir}/ack2; perl Makefile.PL;
 
 ${gitdir}/ack2/ack-standalone: ${gitdir}/ack2/Makefile
@@ -13,6 +13,9 @@ ${gitdir}/ack2/ack-standalone: ${gitdir}/ack2/Makefile
 ${gitdir}/file-next:
 	git clone https://github.com/petdance/file-next.git ${gitdir}/file-next
 
-$(DEPDIR)/File-Next:
+$(DEPDIR)/File-Next: ${gitdir}/file-next
 	cd ${gitdir}/file-next; perl Makefile.PL; make install
 
+.PHONY: install
+install:
+	ln -s $(HOME)/bin/ack ${gitdir}/ack2/ack-standalone
