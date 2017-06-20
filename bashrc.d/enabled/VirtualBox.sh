d63ef349ced6740fca7b8f4930babfc1cfd50806
Packy Anderson <packy@cpan.org>
2017-06-19 23:07:05 -0400

    Add a spinner submodule and modify the vbox start/stop functions to use it while waiting for things to start/stop.

--- bashrc.d/enabled/VirtualBox.sh
+++ bashrc.d/enabled/VirtualBox.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 
+source $HOME/.bashrc.d/enabled/spinner.sh
+
 if [[ -d /Applications/VirtualBox.app ]]; then
     VirtualBox_Contents=/Applications/VirtualBox.app/Contents
     VirtualBoxVM_Contents=$VirtualBox_Contents/Resources/VirtualBoxVM.app/Contents
@@ -33,6 +35,12 @@ EOF
     alias VBoxManage=$VBoxManage
     alias vbox=$VBoxManage
 
+    function vbox-vm-is-booted () {
+        local VM="$1"
+        local GuestIP="/VirtualBox/GuestInfo/Net/0/V4/IP"
+        $VBoxManage guestproperty get "$VM" $GuestIP |& grep -q 'Value: '
+    }
+
     function vbox-log-file () {
         local VM="$1"
         local LOGDIR=$VBoxLog/$VM
@@ -42,19 +50,40 @@ EOF
 
     function vbox-headless () {
         local VM="$1"
-        local LOG=$(vbox-log-file $VM)
-        echo Logging to $LOG
-        vbox-logrotate $LOG
-        date +'== Started %Y-%m-%d %H:%M:%S ==' >> $LOG
-        (nohup bash -c "$VBoxHeadless --startvm \"$VM\" 2>&1" >> $LOG &)
+        if ! vbox-running | grep -q $VM; then
+            local LOG=$(vbox-log-file $VM)
+            echo Logging to $LOG
+            vbox-logrotate $LOG
+            date +'== Started %Y-%m-%d %H:%M:%S ==' >> $LOG
+            (nohup bash -c "$VBoxHeadless --startvm \"$VM\" 2>&1" >> $LOG &)
+
+            # wait for the guest to have an IPv4 address
+            start_spinner "Starting vm $VM..."
+            while ! vbox-vm-is-booted "$VM"; do
+                sleep 0.5
+            done
+            stop_spinner $?
+        else
+            (>&2 echo $VM is already running)
+        fi
     }
 
     function vbox-off () {
         local VM="$1"
-        local LOG=$(vbox-log-file $VM)
-        echo Powering off... | tee -a $LOG
-        $VBoxManage controlvm "$VM" acpipowerbutton |& tee -a $LOG
-        date +'== Stopped %Y-%m-%d %H:%M:%S ==' >> $LOG
+        if vbox-running | grep -q $VM; then
+            local LOG=$(vbox-log-file $VM)
+            echo Powering off... | tee -a $LOG
+            $VBoxManage controlvm "$VM" acpipowerbutton 2>&1 | tee -a $LOG
+            # wait for the guest to have an IPv4 address
+            start_spinner "Stopping vm $VM..."
+            while vbox-running | grep -q "$VM"; do
+                sleep 0.5
+            done
+            stop_spinner $?
+            date +'== Stopped %Y-%m-%d %H:%M:%S ==' >> $LOG
+        else
+            (>&2 echo $VM is not running)
+        fi
     }
 
     function vbox-list () {
---
+++ bashrc.d/enabled/spinner.sh
@@ -0,0 +1,1 @@
+../../submodules/bash-spinner/spinner.sh
