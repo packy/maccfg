0db3f7dffb8bfe54b0e1d0ee4830d0a021b8b89f
Packy Anderson <packy@cpan.org>
2017-07-11 10:20:39 -0400

    Add functions to wrap the commands necessary to tunnle VNC over SSH through my firewall to my Macs at home. Yes, now you know the domain and three of the hostnames I use at home. Woo.

--- bashrc.d/enabled/ssh.sh
+++ bashrc.d/enabled/ssh.sh
@@ -2,3 +2,19 @@
 
 alias ssh-pubkeyinfo='ssh-keygen -lf'
 alias ssh-proxy='ssh -D 8080'
+
+function vnc-ssh-tunnel () {
+  HOST=$1
+  FIREWALL=$2
+  ssh -f -L 5999:$HOST:5900 $FIREWALL "sleep 10"
+  open vnc://localhost:5999
+}
+
+function vnc-ssh-home () {
+  HOST=$1
+  FW=${2:-babs}
+  vnc-ssh-tunnel $HOST.home.packay.com $FW.packay.com
+}
+
+alias vnc-ssh-kirby='vnc-ssh-home kirby'
+alias vnc-ssh-speedy='vnc-ssh-home speedy'
