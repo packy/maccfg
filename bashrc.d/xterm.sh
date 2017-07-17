8edcdf71c97fe060f9f1f001de329f081158f195
Packy Anderson <packy@cpan.org>
2017-07-17 14:40:42 -0400

    More work on automating color selection

--- bashrc.d/xterm.sh
+++ bashrc.d/xterm.sh
@@ -1,5 +1,23 @@
 #!bash # for emacs highlighting
 
+# detect my monitors
+
+function rescan_displays () {
+  local KEY=SPDisplaysDataType
+  export MACOS_DISPLAYS=$(
+    system_profiler $KEY | perl -ne '
+      if (/^(\s+)Displays:/) { $prefix = $1 . q{  }; }
+      if (length $prefix && /^$prefix(\S[^:]+):/) { push @D, $1; }
+      END { print join q{:}, @D; }'
+  )
+}
+
+function display_is () {
+  local DTYPE="$1"
+  [[ "$MACOS_DISPLAYS" == "" ]] && rescan_displays
+  [[ "$MACOS_DISPLAYS" =~ "$DTYPE" ]]
+}
+
 # http://hackipedia.org/Protocols/Terminal,%20DEC%20VT100/html/VT100%20Escape%20Codes.html
 
 function clear () {
@@ -64,10 +82,18 @@ function iTerm2_UnBounceIcon () { iTerm2_1337 RequestAttention=no; }
 function iTerm2_notification () { printf "\033]9;%s\a" "$@"; }
 function iTerm2_titlecolor   () { it2setcolor tab "$@"; }
 function iTerm2_bgcolor      () { it2setcolor bg "$@"; export TERM_BGCOLOR="$@"; }
+alias bgcolor=iTerm2_bgcolor
 
 function iTerm2_cycle_bgcolor () {
   if [[ "$1" == "auto" ]]; then
     while true; do iTerm2_cycle_bgcolor; echo iTerm2_bgcolor $TERM_BGCOLOR; sleep 1; done
+  fi
+  if [[ "$TERM_BGCOLOR_LIST" == "" ]]; then
+    if display_is SE39UY04; then
+      bgcolor_high_list
+    else
+      bgcolor_low_list
+    fi
   fi
   local DEFAULT_LIST DEFAULT_COLOR
   DEFAULT_LIST=000:333:400:600:800:020:030:220:330:440:002:004:008
@@ -90,4 +116,17 @@ function iTerm2_cycle_bgcolor () {
   # we didn't find the current color on the list
   export TERM_BGCOLOR="${array[0]}"
   iTerm2_bgcolor $TERM_BGCOLOR
+}
+alias cycle_bgcolor=iTerm2_cycle_bgcolor
+
+function bgcolor_high_list () {
+  export TERM_BGCOLOR_LIST=000:555:666:600:800:040:050:004:005:006:007:008
+  TERM_BGCOLOR_LIST=$TERM_BGCOLOR_LIST:044:055:066:505:606:707:808:708:809
+  TERM_BGCOLOR_LIST=$TERM_BGCOLOR_LIST:718:627:620:970
+}
+
+function bgcolor_low_list () {
+  export TERM_BGCOLOR_LIST=000:333:400:600:800:020:030:220:330:440:002:004:008
+  TERM_BGCOLOR_LIST=$TERM_BGCOLOR_LIST:022:044:202:404:606:313:424:535:326:548
+  TERM_BGCOLOR_LIST=$TERM_BGCOLOR_LIST:407:515:516:518:627:620:630:640
 }
