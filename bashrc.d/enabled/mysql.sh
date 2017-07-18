653b071600963c508fde03b8fc5236383710431e
Packy Anderson <packy@cpan.org>
2017-07-18 16:02:07 -0400

    Add function to start/stop homebrewed MySQL

--- bashrc.d/enabled/mysql.sh
+++ bashrc.d/enabled/mysql.sh
@@ -3,3 +3,23 @@
 function mysqlg () {
   mysql --defaults-group-suffix=$1
 }
+
+function mysqlctl () {
+  local PLIST=$HOME/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+  case $1 in
+    start)
+      echo Starting MySQL...
+      launchctl load -F $PLIST
+      ;;
+    stop)
+      echo Stopping MySQL...
+      launchctl unload -F $PLIST
+      ;;
+    restart)
+      mysqlctl stop && mysqlctl start
+      ;;
+    *)
+      >&2 echo "Unknown argument \"$1\"; try \"start\", \"stop\", or \"restart\""
+      ;;
+  esac
+}
