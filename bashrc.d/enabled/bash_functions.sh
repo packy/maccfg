6516520efe39f3e755cbe7ae4d7f8d6ab773e255
Packy Anderson <packy@cpan.org>
2017-07-20 12:45:10 -0400

    Add an is_in_list() function to make it easier to work with lists.

--- bashrc.d/enabled/bash_functions.sh
+++ bashrc.d/enabled/bash_functions.sh
@@ -18,3 +18,22 @@ pl2html () {
     set +o monitor
     (sleep 5; rm -f /tmp/$FILENAME.html) &
 }
+
+function is_in_list () {
+  if [[ "$#" -lt 2 ]] || [[ "$#" -gt 3 ]]; then
+    >&2 echo "Usage: is_in_list \"\$LIST\" item [separator]"
+    false
+    return
+  fi
+  LIST="$1" ITEM="$2" SEP=${3:-':'}
+  if [[ "$LIST" != "" ]]; then
+    IFS="$SEP" read -r -a _list <<< "$LIST"
+    for index in "${!_list[@]}"; do
+      if [[ "${_list[index]}" == "$ITEM" ]]; then
+        true
+        return
+      fi
+    done
+  fi
+  false
+}
