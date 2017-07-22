705af584f04e6dba2c86847cb8b8bca25b2d5ca7
Packy Anderson <packy@cpan.org>
2017-07-22 03:15:39 -0400

    Add some URLs to notes, add some dock functions.

--- NOTES.md
+++ NOTES.md
@@ -1,2 +1,7 @@
 # Markdown-Cheatsheet
 * https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+
+# defaults tricks:
+* http://www.defaults-write.com/
+* http://www.defaults-write.com/stop-photos-from-opening-automatically/
+* http://www.defaults-write.com/os-x-make-crash-reporter-appear-as-a-notification/
--- bashrc.d/enabled/Dock.sh
+++ bashrc.d/enabled/Dock.sh
@@ -3,18 +3,32 @@
 function dock-defaults () {
   TYPE=$1; shift
   CMD=$1; shift
-  case $CMD in
-    add) CMD='-array-add' ;;
-  esac
-  defaults write com.apple.dock $TYPE $CMD "$@"
+
+  defaults $CMD com.apple.dock $TYPE "$@"
   killall Dock
 }
 
 function add-spacer-to-dock () {
-  dock-defaults persistent-apps add '{"tile-type"="spacer-tile";}'
+  dock-defaults persistent-apps write -array-add '{"tile-type"="spacer-tile";}'
 }
 
 function add-recent-applications-to-dock () {
-  dock-defaults persistent-others add \
+  dock-defaults persistent-others write -array-add \
     '{ "tile-data" = {"list-type" = 1; }; "tile-type" = "recents-tile";}'
+}
+
+function dock-autohide () {
+  dock-defaults autohide write -float 1
+}
+
+function dock-noautohide () {
+  dock-defaults autohide delete
+}
+
+function dock-autohide-nodelay () {
+  dock-defaults autohide-delay write -float 0
+}
+
+function dock-autohide-delay () {
+  dock-defaults autohide-delay delete
 }
