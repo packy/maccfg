d7e30718a2899106edc0d49249d95eb81a6616fe
Packy Anderson <packy@cpan.org>
2017-09-05 11:25:08 -0400

    More work on automating my laptop's configuration when I wake it in a particular location

--- bashrc.d/enabled/skype.sh
+++ bashrc.d/enabled/skype.sh
@@ -10,14 +10,27 @@ function set-skype-mood () {
     -e "tell application $APP to send command $COMMAND script name $SCRIPT"
 }
 
+function set-skype-status () {
+  printf "Skype: "
+  STATUS=$1
+  APP="\"Skype\""
+  COMMAND="\"set USERSTATUS $STATUS\""
+  SCRIPT="\"AppleScript Status Setter\""
+  osascript \
+    -e "tell application $APP to send command $COMMAND script name $SCRIPT"
+}
+
 function skype-mood-home () {
   set-skype-mood "🏠 🐾 @ Hackensack “Office”"
+  set-skype-status online
 }
 
 function skype-mood-aim () {
   set-skype-mood "⛪ @ 620 Union"
+  set-skype-status online
 }
 
 function skype-mood-pari () {
   set-skype-mood "👩🏻‍⚕️ @ 2-3PM medical appointment"
+  set-skype-status away
 }
--- bashrc.d/enabled/slack.sh
+++ bashrc.d/enabled/slack.sh
@@ -1,9 +1,14 @@
 # thanks to https://gist.github.com/samervin/621fcde93d28346ec5dbab4466b51d45
 # for the example!
 
+function slack_api_url () {
+  METHOD=$1; shift
+  echo "https://slack.com/api/$METHOD?token=$slacktoken"
+}
+
 function slack_status_get () {
   slacktoken=$(cat $HOME/.slack_token)
-  apiurl="https://slack.com/api/users.profile.get?token=$slacktoken"
+  apiurl=$(slack_api_url users.profile.set)
   curl $apiurl
 }
 
@@ -11,23 +16,42 @@ function slack_status_set () {
   EMOJI=$1; shift
   TEXT="$@"
   slacktoken=$(cat $HOME/.slack_token)
-  apiurl="https://slack.com/api/users.profile.set?token=$slacktoken"
+  apiurl=$(slack_api_url users.profile.set)
   curl --silent --data-urlencode "profile={\"status_text\":\"$TEXT\",\"status_emoji\":\"$EMOJI\"}" $apiurl
+}
+
+function slack_presence_set () {
+  TEXT=${1:-auto}
+  slacktoken=$(cat $HOME/.slack_token)
+  apiurl=$(slack_api_url users.setPresence)
+  echo "$apiurl\\&presence=$TEXT"
+  curl $apiurl\&presence=$TEXT
+}
+
+function slack-away () {
+  slack_status_set away > /dev/null
+}
+
+function slack-here () {
+  slack_status_set auto > /dev/null
 }
 
 function slack-status-pari () {
   echo "Setting slack status to 👩🏻‍⚕️ @ 2-3PM medical appointment"
   slack_status_set :female-health-worker:‍ @ 2-3PM medical appointment > /dev/null
+  slack-away
 }
 
 function slack-status-home () {
   echo "Setting slack status to 🏠 (paws) @ Hackensack \“Office\”"
   slack_status_set :house: ':feet: @ Hackensack \"Office\"' > /dev/null
+  slack-here
 }
 
 function slack-status-aim () {
   echo "Setting slack status to ⛪ @ 620 Union"
   slack_status_set :church: @ 620 Union > /dev/null
+  slack-here
 }
 
 function pari () {
