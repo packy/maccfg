59da6c4072c434beef20cdbed2ab96e12f6a440f
Packy Anderson <packy@cpan.org>
2018-04-13 22:09:26 -0400

    Tweaks to my screen resolution wrapper function.

--- bashrc.d/enabled/macos_screen.sh
+++ bashrc.d/enabled/macos_screen.sh
@@ -10,7 +10,13 @@ get_screen_resolution () {
 
 res () {
     case $1 in
-        1920|192)
+        4k|uhd)
+            screenresolution set 3840x2160x32@0
+            ;;
+        2560|wqxga)
+            screenresolution set 2560x1600x32@0
+            ;;
+        1920|192|1080p)
             screenresolution set 1920x1080x32@60
             ;;
         1440|144)
@@ -22,7 +28,7 @@ res () {
         1280h|128h)
             screenresolution set 1280x1024x32@60
             ;;
-        1280|128|1280w|128w)
+        1280|128|1280w|128w|720p)
             screenresolution set 1280x720x32@60
             ;;
         1024|1k)
@@ -31,6 +37,22 @@ res () {
         800)
             screenresolution set 800x600x32@60
             ;;
+        list)
+            screenresolution list
+            echo
+            ;;
+        *)
+            echo Scripted resolutions:
+            echo " + 4k"
+            echo " + 2560"
+            echo " + 1920"
+            echo " + 1680"
+            echo " + 1440"
+            echo " + 1280h"
+            echo " + 1280w"
+            echo " + 1024"
+            echo " + 800"
+            echo ""
+            echo "use \"res list\" to list available resolutions"
     esac
 }
-
