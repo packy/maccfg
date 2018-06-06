7a2bc7d3abf897e51eca5f5c554c41134de99032
Packy Anderson <packy@cpan.org>
2018-06-05 21:53:00 -0400

    Add shell function to enable/disable screencapture dropshadow.

--- bashrc.d/enabled/Finder.sh
+++ bashrc.d/enabled/Finder.sh
@@ -10,4 +10,17 @@ function finder-nohide () {
 
 function finder-screencapture-path () {
   defaults write com.apple.screencapture "$@"
+  killall SystemUIServer
+}
+
+function finder-screencapture-dropshadow () {
+  if [[ "${1^^}" == "ON" ]]; then
+    defaults write com.apple.screencapture disable-shadow -bool false
+    killall SystemUIServer
+  elif [[ "${1^^}" == "OFF" ]]; then
+    defaults write com.apple.screencapture disable-shadow -bool true
+    killall SystemUIServer
+  else
+    >&2 echo "Usage: finder-screencapture-dropshadow <ON|OFF>"
+  fi
 }
