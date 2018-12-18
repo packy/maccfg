e5f3219847e79b47182c7beb67eec510ea39fba2
Packy Anderson <PAnderson@advancedinfomanagement.com>
2018-12-18 11:31:06 -0500

    Add function to indent the contents of the paste buffer

--- bashrc.d/enabled/aim.sh
+++ bashrc.d/enabled/aim.sh
@@ -1,3 +1,8 @@
 #!bash # for emacs formatting
 
 re_source_file $HOME/.bash_aim # work files
+
+# function to indent the contents of the paste buffer N spaces
+pbindent () {
+  pbpaste | INDENT=$1 perl -pe 's/^/" "x$ENV{INDENT}/e' | pbcopy
+}
