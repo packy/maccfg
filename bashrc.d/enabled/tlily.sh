d3a53e85e31cb423c2cd2572a9ffff7a2ef0f7dd
Packy Anderson <PAnderson@advancedinfomanagement.com>
2019-03-14 15:42:14 -0400

    Use xrun more effectively to launch tlily.

--- bashrc.d/enabled/tlily.sh
+++ bashrc.d/enabled/tlily.sh
@@ -4,11 +4,9 @@ export PERLBREW_BIN=$( perl -e '$ENV{PERLBREW_PATH} =~ s/^.*:([^:]+)$/$1/; print
 
 tlily () {
     if [ -x $PERLBREW_BIN/tlily ]; then
-        OUTPUT=$()
-        xrun "source $HOME/.bash_functions; set_term_profile tlily; xsize 70 80; xtitle 'packy @ RPI'; $PERLBREW_BIN/tlily"
+        xrun --title 'packy @ RPI' --height 70 "$PERLBREW_BIN/tlily"
     else
         echo Tigerlily is not installed at $PERLBREW_BIN/tlily
         return 1
     fi
 }
-
