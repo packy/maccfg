b909b91881583baaa1b19424561016e907a66a31
Packy Anderson <packy@cpan.org>
2022-08-23 16:17:06 -0400

    Fix aliases for JetBrains tools

--- bashrc.d/enabled/JetBrains.sh
+++ bashrc.d/enabled/JetBrains.sh
@@ -1,4 +1,3 @@
 #!bash # for emacs highlighting
-
-alias pstorm=$(type -p phpstorm)
-alias charm=$(type -p pycharm)
+[[ ! -x /usr/local/bin/pstorm ]] && alias pstorm=$(type -p phpstorm)
+[[ ! -x /usr/local/bin/charm ]] && alias charm=$(type -p pycharm)
