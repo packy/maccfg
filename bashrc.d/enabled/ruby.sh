2e338531b84724841bb5984bc42d31bee8703064
Packy Anderson <packy@cpan.org>
2022-08-23 16:35:51 -0400

    Tweak tab completion

--- bashrc.d/enabled/ruby.sh
+++ bashrc.d/enabled/ruby.sh
@@ -4,4 +4,5 @@ unshift_path /usr/local/lib/ruby/gems/2.6.0/bin
 unshift_path /usr/local/opt/ruby/bin
 
 # find any gems that have tab completion defined
-source $(find /usr/local/lib/ruby/gems -name tab_complete.sh)
+TAB_COMPLETE=$(find /usr/local/lib/ruby/gems -name tab_complete.sh 2>/dev/null)
+[[ ! -z "$TAB_COMPLETE" ]] && source  $TAB_COMPLETE
