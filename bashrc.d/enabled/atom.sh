07605ab3a99574d1efd2cf2ac8892aa008c28911
Packy Anderson <PackyAnderson@gmail.com>
2022-09-23 20:30:41 -0400

    Allow atom to sync settings through work proxy

--- bashrc.d/enabled/atom.sh
+++ bashrc.d/enabled/atom.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # for syntax highlightling in emacs, mostly
 
-alias a=/Applications/Atom.app/Contents/Resources/app/atom.sh
+# to get through proxy server
+# https://github.com/atom/settings-view/issues/1063#issuecomment-419444171
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+
+function atom {
+  /Applications/Atom.app/Contents/Resources/app/atom.sh --proxy-server=http://genproxy:8080
+}
+alias a=atom
