3581dcbde5396f5db6eef2450fa8e8d90727b25c
Packy Anderson <PackyAnderson@gmail.com>
2023-04-12 16:31:45 -0400

    RIP Atom 😢

--- bashrc.d/disabled/atom.sh
+++ bashrc.d/disabled/atom.sh
@@ -6,6 +6,6 @@
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 function atom {
-  /Applications/Atom.app/Contents/Resources/app/atom.sh --proxy-server=http://genproxy:8080
+  /Applications/Atom.app/Contents/Resources/app/atom.sh --proxy-server=http://genproxy:8080 $@
 }
 alias a=atom
