28387a15baec7920797f255ed8b49083da412607
Packy Anderson <packy@cpan.org>
2023-11-29 23:26:09 -0500

    Fix things I had at one point in my pwc script...

--- pwc/pwc.sh
+++ pwc/pwc.sh
@@ -21,13 +21,20 @@ function pwc () {
   git merge upstream/master
   git push
 
+  # find the latest challenge directory
+  CHALLENGE_DIR=$(ls -d challenge-* | tail -1)
+  cd $CHALLENGE_DIR
+
   # set up the skeleton files
+  mkdir raku
   pwc_skeleton $CFGDIR/pwc/skeleton.raku raku/ch-1.raku
   pwc_skeleton $CFGDIR/pwc/skeleton.raku raku/ch-2.raku
 
+  mkdir perl
   pwc_skeleton $CFGDIR/pwc/skeleton.pl perl/ch-1.pl
   pwc_skeleton $CFGDIR/pwc/skeleton.pl perl/ch-2.pl
 
+  mkdir python
   pwc_skeleton $CFGDIR/pwc/skeleton.py python/ch-1.py
   pwc_skeleton $CFGDIR/pwc/skeleton.py python/ch-2.py
 
